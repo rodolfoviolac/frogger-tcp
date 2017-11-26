99cb14ae787f167e603fda2fae4a76d0910b842b
Rodolfo Viola <rodolfoviolac@gmail.com>
2017-11-26 11:39:15 -0200

    Add assets and simple frog move

--- core/src/com/frogger/game/FroggerGame.java
+++ core/src/com/frogger/game/FroggerGame.java
@@ -2,32 +2,51 @@ package com.frogger.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FroggerGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Sprite player;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("frog.jpg");
+		player = new Sprite(new Texture("frog.jpg"));
+
+
 	}
 
 	@Override
 	public void render () {
+
+		if(Gdx.input.isKeyPressed(Input.Keys.W)){
+		player.setY(player.getY() + 10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.S)){
+		player.setY(player.getY() - 10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.A)){
+		player.setX(player.getX() - 10);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.D)){
+		player.setX(player.getX() + 10);
+		}
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(player, player.getX(), player.getY());
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		player.getTexture().dispose();
 	}
 }
