3822ebcd368e8b46d38eecfc187c86d6d6bae574
unknown <SuportePPGE@PPGE01.portalppge.ufrgs.br>
2017-12-13 19:08:29 -0200

    remoção de linhas inutilizadas

--- core/src/com/frogger/game/screens/MainGameScreen.java
+++ core/src/com/frogger/game/screens/MainGameScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
